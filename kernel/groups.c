3792c40d170d3ff44a997c4b8c905f12a6e8fe93
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2017-07-10 15:51:17 -0700

    kernel/groups.c: use sort library function
    setgroups is not exactly a hot path, so we might as well use the library
    function instead of open-coding the sorting.  Saves ~150 bytes.
    
    Link: http://lkml.kernel.org/r/1497301378-22739-1-git-send-email-linux@rasmusvillemoes.dk
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Matthew Wilcox <mawilcox@microsoft.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: ISNing <ISNing_SF@outlook.com>

--- kernel/groups.c
+++ kernel/groups.c
@@ -5,6 +5,7 @@
 #include <linux/export.h>
 #include <linux/slab.h>
 #include <linux/security.h>
+#include <linux/sort.h>
 #include <linux/syscalls.h>
 #include <asm/uaccess.h>
 
@@ -102,33 +103,18 @@ static int groups_from_user(struct group_info *group_info,
 	return 0;
 }
 
-/* a simple Shell sort */
+static int gid_cmp(const void *_a, const void *_b)
+{
+	gid_t a = *(gid_t *)_a;
+	gid_t b = *(gid_t *)_b;
+
+	return (a > b) - (a < b);
+}
+
 static void groups_sort(struct group_info *group_info)
 {
-	int base, max, stride;
-	int gidsetsize = group_info->ngroups;
-
-	for (stride = 1; stride < gidsetsize; stride = 3 * stride + 1)
-		; /* nothing */
-	stride /= 3;
-
-	while (stride) {
-		max = gidsetsize - stride;
-		for (base = 0; base < max; base++) {
-			int left = base;
-			int right = left + stride;
-			gid_t tmp = GROUP_AT(group_info, right);
-
-			while (left >= 0 && GROUP_AT(group_info, left) > tmp) {
-				GROUP_AT(group_info, right) =
-				    GROUP_AT(group_info, left);
-				right = left;
-				left -= stride;
-			}
-			GROUP_AT(group_info, right) = tmp;
-		}
-		stride /= 3;
-	}
+	sort(group_info->blocks[0], group_info->ngroups, sizeof(*group_info->blocks[0]),
+	     gid_cmp, NULL);
 }
 
 /* a simple bsearch */
